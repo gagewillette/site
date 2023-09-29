29e42f6900f0134169b12d4c7adfe348cead343c
gagewillette <79120652+gagewillette@users.noreply.github.com>
2023-09-29 07:36:48 -0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+import { Dropdown } from "@mui/base/Dropdown";
+import { MenuButton } from "@mui/base/MenuButton";
+import { Menu } from "@mui/base/Menu";
+import { MenuItem } from "@mui/base/MenuItem";
+
+function App() {
+  const [count, setCount] = useState(1);
+
+
+  const createHandleMenuClick = (menuItem: string) => {
+    return () => {
+      console.log(`Clicked on ${menuItem}`);
+    };
+  };
+
+  return (
+    <>
+      <div className="header">
+        <div className="header_container">
+          <div className="header_name_container">
+            <h1>Gage Willette's</h1>
+            <Dropdown
+            >
+              <MenuButton>_______</MenuButton>
+              <Menu
+                slotProps={{
+                  listbox: { className: 'CustomMenuIntroduction--listbox' },
+                }}
+              >
+                <MenuItem className="trading-item customMenuItem">
+                  Trading
+                </MenuItem>
+                <MenuItem className="coding-item customMenuItem">
+                  Coding
+                </MenuItem>
+                <MenuItem className="youtube-item customMenuItem">
+                  YouTube
+                </MenuItem>
+              </Menu>
+            </Dropdown>
+            <h1>Blog</h1>
+          </div>
+          <div className="header_button_container">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+          </div>
+        </div>
+
+        <hr className="bottom_linereak"></hr>
+      </div>
+    </>
+  );
+}
+
+export default App;
